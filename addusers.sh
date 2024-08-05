208448b6f0c6c6b53feac1b888e1bf041a41e1a5
DevOps Team <134445691+azureDevOpsTeam@users.noreply.github.com>
2024-08-05 11:51:33 +0330

    Create addusers.sh

---
+++ addusers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Array od Username and passwords
+declare -A users
+users = (
+  [baran]=Aa123456@
+  [laleh]=Aa123456@
+  [jaleh]=Aa123456@
+  [hamed]=Aa123456@
+  [mansouri-pedar]=Aa123456@
+  [mansouri-madar]=Aa123456@
+  [mansouri-khaleh]=Aa123456@
+  [meysam]=Aa123456@
+  [sadaf]=Aa123456@
+  [reza]=Aa123456@
+  [narges]=Aa123456@
+  [zahra]=Aa123456@
+  [fatemeh]=Aa123456@
+)
+for username in "${!users[@]}"; do
+  sudo useradd -m -s /bin/bash "$username"
+  echo "${username}:${users[$username]}" | sudo chpasswd
+done
